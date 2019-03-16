2109569f7139bd62f1101c4fc396bb8ac208faef
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2019-03-16 08:11:06 -0400

    20190316 08:11

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // get the routes
-require("./routing/apiRoutes.js")(app);
-require("./routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 // listener:
 app.listen(PORT, function() {
